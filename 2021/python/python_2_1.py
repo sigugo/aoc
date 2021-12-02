667aa87d3d35588d1583594d218ee53f65442649
sigugo <tprell@gmail.com>
2021-12-02 06:51:09 +0100

    Day 2

--- 2021/python/python_2_1.py
+++ 2021/python/python_2_1.py
@@ -1,9 +1,38 @@
 from tools import file_to_list
 
+class Submarine:
+    def __init__(self):
+        self._x: int = 0
+        self._y: int = 0
+        self._course: list[tuple[str, int]] = []
 
+    def append_course_file(self, file_name: str) -> None:
+        file_data = file_to_list(file_name)
+        for entry in file_data:
+            direction = entry.split(" ")[0].strip()
+            distance = entry.split(" ")[1].strip()
+            self._course.append((direction, int(distance)))
 
+    def follow_course(self):
+        for command in self._course:
+            if command[0] == "forward":
+                self._x += command[1]
+            elif command[0] == "down":
+                self._y += command[1]
+            elif command[0] == "up":
+                self._y -= command[1]
+                if self._y <= 0:
+                    self._y = 0
+
+    def print_position(self) -> None:
+        print("Distance:", self._x)
+        print("Depth:", self._y)
+        print("AOC Result", self._x * self._y)
 
 
 if __name__ == '__main__':
-    input_file = '../inputs/1_1/input.txt'
-
+    input_file = '../inputs/2/input.txt'
+    sub = Submarine()
+    sub.append_course_file(input_file)
+    sub.follow_course()
+    sub.print_position()
--- 2021/python/python_2_2.py
+++ 2021/python/python_2_2.py
@@ -1,10 +1,29 @@
-from tools import file_to_list
+import python_2_1
 
 
+class Submarine(python_2_1.Submarine):
+    def __init__(self):
+        super(Submarine, self).__init__()
+        self._aim: int = 0
 
+    def follow_course(self):
+        for command in self._course:
+            if command[0] == "forward":
+                self._x += command[1]
+                if self._aim != 0:
+                    self._y += self._aim * command[1]
+                    if self._y <= 0:
+                        self._y = 0
+            else:
+                if command[0] == "down":
+                    self._aim += command[1]
+                elif command[0] == "up":
+                    self._aim -= command[1]
 
 
 if __name__ == '__main__':
-    input_file = '../inputs/1_1/input.txt'
-    print(sweep.count_sliding_depth_increases())
-
+    input_file = '../inputs/2/input.txt'
+    sub = Submarine()
+    sub.append_course_file(input_file)
+    sub.follow_course()
+    sub.print_position()
