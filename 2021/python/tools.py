aab5e47437bf191eae1ad0e86a380c753b5e09ab
sigugo <tprell@gmail.com>
2021-12-03 11:04:51 +0100

    Day 3

--- 2021/python/tools.py
+++ 2021/python/tools.py
@@ -1,5 +1,4 @@
-
-
+# Read input data into list of strings
 def file_to_list(file_name: str) -> list:
     output = []
     with open(file_name, 'r') as file:
@@ -9,3 +8,15 @@ def file_to_list(file_name: str) -> list:
                     output.append(line)
     return output
 
+# given a list of strings with equal length, count the occurenc of a character in the whole list at the given string index
+def count_char_at_index(data: list[str], char: str, index: int) -> int:
+    count: int = 0
+    for string_value in data:
+        if string_value[index] == char:
+            count += 1
+    return count
+
+# XOR binary invert
+def binary_invert(binary: str) -> str:
+    return bin(int(binary, 2) ^ (2 ** (len(binary) + 1) - 1))[3:]
+ 
