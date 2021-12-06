d9131043f3d890b35f41356c8d316fbd74ba890e
sigugo <tprell@gmail.com>
2021-12-06 08:52:30 +0100

    Refactor 6.2

--- 2021/python/day_6_2.py
+++ 2021/python/day_6_2.py
@@ -8,11 +8,8 @@ class SchoolOfFish:
         self._swarm: list[int] = [0] * (self._spawn_timer + self._mature_delta + 1)
 
     def parse_input_data(self, input_data: list[str]):
-        input_strings: list[str] = []
-        for line in input_data:
-            input_strings.extend(line.strip().split(","))
-        for entry in input_strings:
-            self._swarm[int(entry)] += 1
+        for value in input_data.strip().split(","):
+            self._swarm[int(value)] += 1
 
     def count_fish(self) -> None:
         count: int = 0
@@ -53,6 +50,6 @@ if __name__ == "__main__":
     # input_file = "../inputs/6/example.txt"
     input_data: list[str] = file_to_list(input_file)
     lanternfish_school = SchoolOfFish()
-    lanternfish_school.parse_input_data(input_data)
+    lanternfish_school.parse_input_data(input_data[0])
     lanternfish_school.advance_day(256)
     print(lanternfish_school.count_fish())
