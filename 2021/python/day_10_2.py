6169187e7002d9f6f9d3b144243c93a86442d33c
Thorsten Prell <tprell@gmail.com>
2021-12-09 12:22:12 +0100

    Delete day_10_2.py

--- 2021/python/day_10_2.py
+++
@@ -1,7 +0,0 @@
-from tools import *
-
-
-if __name__ == "__main__":
-    #input_file = "../inputs/x/data.input"
-    input_file = "../inputs/x/data.example"
-    input_data: list[str] = file_to_list(input_file)
