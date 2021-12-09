8091ef163928c6b295d8204349bdb22c4f9dce2f
sigugo <tprell@gmail.com>
2021-12-09 11:43:48 +0100

    Day 9

--- 2021/python/day_9_1.py
+++ 2021/python/day_9_1.py
@@ -1,7 +1,6 @@
 from tools import *
 
 
-
 class CaveMap:
     def __init__(self):
         self._min_val = 0
@@ -22,26 +21,27 @@ class CaveMap:
             sum += self._get_value_at_coordinate(x, y) + 1
         return sum
 
-
     def find_lowpoints(self) -> list[tuple[int, int]]:
         lowpoints: list[tuple[int, int]] = []
         for y in range(len(self._map)):
             for x in range(len(self._map[y])):
-                is_lowpoint = True
-                if self._get_value_at_coordinate(x+1, y) <= self._get_value_at_coordinate(x, y):
-                    is_lowpoint = False
-                if self._get_value_at_coordinate(x-1, y) <= self._get_value_at_coordinate(x, y):
-                    is_lowpoint = False
-                if self._get_value_at_coordinate(x, y+1) <= self._get_value_at_coordinate(x, y):
-                    is_lowpoint = False
-                if self._get_value_at_coordinate(x, y-1) <= self._get_value_at_coordinate(x, y):
-                    is_lowpoint = False
-                if is_lowpoint:
-                    print('found low at', x, y)
+                if self._is_lowpoint(x, y):
                     lowpoints.append((x, y))
-        return(lowpoints)
-    
-    def _get_value_at_coordinate(self, x, y, debug = 0):
+        return lowpoints
+
+    def _is_lowpoint(self, x, y) -> bool:
+        is_lowpoint = True
+        if self._get_value_at_coordinate(x + 1, y) <= self._get_value_at_coordinate( x, y):
+            is_lowpoint = False
+        if self._get_value_at_coordinate(x - 1, y) <= self._get_value_at_coordinate( x, y):
+            is_lowpoint = False
+        if self._get_value_at_coordinate(x, y + 1) <= self._get_value_at_coordinate( x, y):
+            is_lowpoint = False
+        if self._get_value_at_coordinate(x, y - 1) <= self._get_value_at_coordinate( x, y):
+            is_lowpoint = False
+        return is_lowpoint
+
+    def _get_value_at_coordinate(self, x, y, debug=0):
         value: int = self._max_val
         if debug > 1:
             print("* Getting value for:", x, y)
@@ -49,12 +49,13 @@ class CaveMap:
             if 0 <= x < len(self._map[y]):
                 value = self._map[y][x]
             elif debug > 0:
-                print('out of bounds x', x, y)
+                print("out of bounds x", x, y)
         elif debug > 0:
-                print('out of bounds y', x, y)
-        if debug > 1:    
-            print('value returned', value)
+            print("out of bounds y", x, y)
+        if debug > 1:
+            print("value returned", value)
         return value
+
 
 if __name__ == "__main__":
     input_file = "../inputs/9/data.input"
---
+++ 2021/python/day_9_2.py
@@ -0,0 +1,100 @@
+from tools import *
+import day_9_1
+
+
+class CaveMap(day_9_1.CaveMap):
+    def __init__(self, input_data):
+        super(CaveMap, self).__init__()
+
+        self.parse_input_data(input_data)
+
+        self._basin_map: list[list[int]] = []
+        self._no_basin_index: int = 0
+        self._basin_index: int = self._no_basin_index
+
+        self._initialize_basin_map()
+
+    def _initialize_basin_map(self) -> None:
+        for y in self._map:
+            basin_x = []
+            for x in y:
+                basin_x.append(self._no_basin_index)
+            self._basin_map.append(basin_x)
+
+    def map_basins(self) -> None:
+        for y in range(len(self._map)):
+            for x in range(len(self._map[y])):
+                self._walk_basin(x, y)
+
+    def _walk_basin(self, x: int = 0, y: int = 0, new_basin: bool = True):
+        point_basin_index = self._get_basin_index_at_coordinate(x, y)
+        is_basin_point = self._is_basin_point(x, y)
+        if point_basin_index == self._no_basin_index and is_basin_point:
+            if new_basin and self._get_max_basin_index() == self._basin_index:
+                self._basin_index += 1
+            self._set_basin_index_at_coordinate(x, y, self._basin_index)
+            self._walk_basin(x + 1, y, False)
+            self._walk_basin(x - 1, y, False)
+            self._walk_basin(x, y + 1, False)
+            self._walk_basin(x, y - 1, False)
+
+    def _get_basin_index_at_coordinate(self, x, y, debug=0) -> int:
+        value: int = self._no_basin_index
+        if debug > 1:
+            print("* Getting value for:", x, y)
+        if 0 <= y < len(self._basin_map):
+            if 0 <= x < len(self._basin_map[y]):
+                value = self._basin_map[y][x]
+            elif debug > 0:
+                print("out of bounds x", x, y)
+        elif debug > 0:
+            print("out of bounds y", x, y)
+        if debug > 1:
+            print("value returned", value)
+        return value
+
+    def _set_basin_index_at_coordinate(self, x, y, i) -> None:
+        self._basin_map[y][x] = i
+
+    def _is_basin_point(self, x, y) -> bool:
+        if self._get_value_at_coordinate(x, y) == self._max_val:
+            return False
+        return True
+
+    def _get_max_basin_index(self) -> int:
+        max_basin_index = self._no_basin_index
+        for y in range(len(self._map)):
+            for x in range(len(self._map[y])):
+                point_basin_index = self._get_basin_index_at_coordinate(x, y)
+                if point_basin_index > max_basin_index:
+                    max_basin_index = point_basin_index
+        return max_basin_index
+
+    def get_largest_basins_product(self, count: int = 3) -> int:
+        basin_sizes = self._get_basin_sizes_list()
+        basin_sizes.sort(reverse=True)
+        product = 1
+        for n in basin_sizes[:count]:
+            product *= n
+        return product
+
+    def _get_basin_sizes_list(self) -> list[int]:
+        size_dict: dict[int, int] = {}
+        size_list: list[int] = []
+        for y in range(len(self._map)):
+            for x in range(len(self._map[y])):
+                index = self._get_basin_index_at_coordinate(x, y)
+                if index > 0:
+                    size_dict[index] = size_dict.setdefault(index, 0) + 1
+        for value in size_dict.values():
+            size_list.append(value)
+        return size_list
+
+
+if __name__ == "__main__":
+    input_file = "../inputs/9/data.input"
+    # input_file = "../inputs/9/data.example"
+    input_data: list[str] = file_to_list(input_file)
+    map = CaveMap(input_data)
+    map.map_basins()
+    print(map.get_largest_basins_product())
