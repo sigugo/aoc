8eb3492cc132a04b595151e85082bbb589ed5b0b
sigugo <tprell@gmail.com>
2021-12-12 15:49:38 +0100

    Day 12

--- 2021/python/day_12_2.py
+++ 2021/python/day_12_2.py
@@ -1,23 +1,18 @@
 from tools import *
 from dataclasses import dataclass, field
 from copy import deepcopy
+import day_12_1
 
 
 @dataclass
-class CavePathMapper:
-    marker: str
-    dataset: field(default_factory=dict[str, list[str]])
-    result: field(default_factory=list[str])
+class CavePathMapper(day_12_1.CavePathMapper):
+    revisit_allowed: bool
 
 
-class CaveSystem:
+class CaveSystem(day_12_1.CaveSystem):
     def __init__(self, input_data: list[str] = []):
-        self._caves: dict[str, list[str]] = {}
-        self._paths: list[list[str]] = []
-        self._start = "start"
-        self._end = "end"
-        if input_data:
-            self.parse_input(input_data)
+        self._revisit_allowed: bool = True
+        super(CaveSystem, self).__init__(input_data)
 
     def parse_input(self, input_data: list[str]) -> None:
         split_char = "-"
@@ -28,11 +23,20 @@ class CaveSystem:
             if b != self._end and a != self._start:
                 self._caves.setdefault(b, []).append(a)
         self._walk_paths()
-        self._paths.sort()
+        paths: list[str] = []
+        for path in self._paths:
+            paths.append(",".join(path))
+        paths = list(dict.fromkeys(paths))
+        paths.sort()
+        self._paths = []
+        for path in paths:
+            self._paths.append(path.split(","))
 
     def _walk_paths(self, log_level: int = 0):
         caves: list[CavePathMapper] = [
-            CavePathMapper(self._start, deepcopy(self._caves), [self._start]),
+            CavePathMapper(
+                self._start, deepcopy(self._caves), [self._start], self._revisit_allowed
+            ),
         ]
         keep_walking = True
         while keep_walking:
@@ -43,7 +47,8 @@ class CaveSystem:
                     print("\n## Current Cave")
                     print(" YOU ARE HERE:", cave.marker)
                     print(" Visited:", cave.result)
-                    print(" Location Map:", type(cave.dataset))
+                    print(" Revisit allowed:", cave.revisit_allowed)
+                    print(" Location Map:")
                     for k, v in list(cave.dataset.items()):
                         print("  ", k, "=>", v)
                 if cave.marker == self._end:
@@ -51,8 +56,32 @@ class CaveSystem:
                 else:
                     keep_walking = True
                     if cave.marker.islower():
+                        if (
+                            cave.revisit_allowed
+                            and cave.marker != self._start
+                            and cave.marker != self._end
+                        ):
+                            next_caves = cave.dataset[cave.marker]
+                            if len(next_caves) > 0:
+                                for next_cave in next_caves:
+                                    next_visited_locations: list[str] = deepcopy(
+                                        cave.result
+                                    ) + [next_cave]
+                                    temp_caves.append(
+                                        CavePathMapper(
+                                            next_cave,
+                                            deepcopy(cave.dataset),
+                                            next_visited_locations,
+                                            False,
+                                        )
+                                    )
                         if log_level > 0:
-                            print(" - removing", cave.marker)
+                            print(
+                                " - removing",
+                                cave.marker,
+                                "with repeat",
+                                cave.revisit_allowed,
+                            )
                         next_caves = cave.dataset.pop(cave.marker)
                         for k in cave.dataset.keys():
                             if cave.marker in cave.dataset[k]:
@@ -75,28 +104,17 @@ class CaveSystem:
                                     next_cave,
                                     deepcopy(cave.dataset),
                                     next_visited_locations,
+                                    deepcopy(cave.revisit_allowed),
                                 )
                             )
             caves = deepcopy(temp_caves)
         for cave in caves:
             self._paths.append(cave.result)
 
-    def get_path_count(self) -> int:
-        return len(self._paths)
-
-    def __str__(self) -> str:
-        output = ""
-        for path in self._paths:
-            line = ""
-            for cave in path:
-                line += cave + ","
-            output += line[:-1] + "\n"
-        return output
-
 
 if __name__ == "__main__":
-    # input_file = "../inputs/12/data.input"
-    input_file = "../inputs/12/data.example"
+    input_file = "../inputs/12/data.input"
+    # input_file = "../inputs/12/data.example"
     input_data: list[str] = file_to_list(input_file)
     cave_system = CaveSystem(input_data)
     print(cave_system.get_path_count())
