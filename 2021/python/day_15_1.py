4bc4b686cce98f4bfa45557928fa453cdc94cc44
sigugo <tprell@gmail.com>
2021-12-15 22:58:45 +0100

    Day 15.1 why exactly did I not think of this earlier?

--- 2021/python/day_15_1.py
+++ 2021/python/day_15_1.py
@@ -23,54 +23,50 @@ def get_2d_matrix_value_at_x_y_safe(
 class PathRisk:
     def __init__(self, input_data: list[str] = []):
         self._riskmap: list[list[int]] = []
+        self._costmap: list[list[int]] = []
         self._exit: tuple[int, int] = ()
-        self._paths: list[
-            tuple[int, int, int, list[tuple[int, int]]]
-        ] = []  # cost, pos_x, pos_y
+        self._paths: list[tuple[int, int, int]] = []
         self._cheapest_path_value: int = 0
         if input_data:
             self.parse_input(input_data)
 
     def parse_input(self, input: list[str]) -> None:
+        max_field_cost: int = 9
         for line in input:
             self._riskmap.append(list(map(int, list(line))))
         ym: int = len(self._riskmap) - 1
         xmym: int = len(self._riskmap[ym]) - 1
+        for i in range(ym + 1):
+            line: list[int] = []
+            for j in range(len(self._riskmap[i])):
+                line.append(max_field_cost * (i + j))
+            self._costmap.append(line)
         self._exit = (xmym, ym)
         for i in range(1, len(self._riskmap) - 1):
             self._cheapest_path_value += self._riskmap[i][0]
         for j in range(xmym + 1):
             self._cheapest_path_value += self._riskmap[ym][j]
-        self._paths.append((0, 0, 0, [(0, 0)]))
-        self._cheapest_path_value = 690
+        self._paths.append((0, 0, 0))
         self._get_cheapest_path()
 
     def _get_cheapest_path(self) -> None:
         iter = self._cheapest_path_value
         while True:
             paths: list[tuple[int, int, int]] = []
-            for cost, x, y, visited in self._paths:
+            for cost, x, y in self._paths:
                 if not self._at_exit(x, y):
-                    # for xn, yn in [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]:
-                    for xn, yn in [(x, y + 1), (x + 1, y)]:
-                        if (xn, yn) not in visited:
-                            next_value = self._get_value(xn, yn)
-                            next_cost = cost + next_value
-                            if (
-                                next_value > 0
-                                and next_cost
-                                <= self._cheapest_path_value + self._steps_to_exit(x, y)
-                            ):
-                                paths.append((next_cost, xn, yn, visited + [(x, y)]))
-                                if self._at_exit(xn, yn):
-                                    print("Next Step is an exit with value", next_cost)
+                    for xn, yn in [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]:
+                        next_value = self._get_value(xn, yn)
+                        next_cost = cost + next_value
+                        if next_value > 0 and next_cost <= self._cheapest_path_value:
+                            if next_cost < self._get_lowest_cost(xn, yn):
+                                paths.append((next_cost, xn, yn))
+                                self._set_lowest_cost(xn, yn, next_cost)
                                 if (
                                     self._at_exit(xn, yn)
                                     and next_cost < self._cheapest_path_value
                                 ):
                                     self._cheapest_path_value = next_cost
-            print(self._cheapest_path_value)
-            print(len(self._paths))
             if len(paths) == 0:
                 print(self._cheapest_path_value)
                 break
@@ -80,12 +76,15 @@ class PathRisk:
     def _at_exit(self, x: int, y: int):
         return (x, y) == self._exit
 
-    def _steps_to_exit(self, x, y):
-        return self._exit[0] - x + self._exit[1] - y
-
     def _get_value(self, x: int, y: int) -> int:
         default: int = 0
         return get_2d_matrix_value_at_x_y_safe(self._riskmap, x, y)
+
+    def _get_lowest_cost(self, x, y) -> int:
+        return self._costmap[y][x]
+
+    def _set_lowest_cost(self, x, y, cost) -> None:
+        self._costmap[y][x] = cost
 
     def __str__(self) -> str:
         output = ""
