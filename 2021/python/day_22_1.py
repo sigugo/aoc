5939f01c6636ef6ebdd5fbb596494078238741a1
sigugo <tprell@gmail.com>
2021-12-22 13:38:39 +0100

    Day 22.1 too much memory

--- 2021/python/day_22_1.py
+++ 2021/python/day_22_1.py
@@ -1,6 +1,6 @@
 from tools import file_to_list
 from typing import NamedTuple
-
+from functools import cache
 
 MIN_AXIS: int = -50
 MAX_AXIS: int = 50
@@ -15,36 +15,37 @@ class Point(NamedTuple):
 
 class Command(NamedTuple):
     on: bool
-    xrange: list[int]
-    yrange: list[int]
-    zrange: list[int]
+    xrange: tuple[int, int]
+    yrange: tuple[int, int]
+    zrange: tuple[int, int]
 
     @classmethod
+    @cache
     def from_str(cls, data: str):
         command, coordranges = data.split(" ")
-        xmm, ymm, zmm = [
-            sorted(
-                [
-                    int(coordrange[2:].split("..")[0]),
-                    int(coordrange[2:].split("..")[1]),
-                ]
-            )
-            for coordrange in coordranges.split(",")
-        ]
+
         return cls(
             True if command == ON_COMMAND else False,
-            [x for x in range(xmm[0], xmm[1] + 1)],
-            [y for y in range(ymm[0], ymm[1] + 1)],
-            [z for z in range(zmm[0], zmm[1] + 1)],
+            *[ 
+                tuple(
+                    sorted(
+                        [
+                            int(coordrange[2:].split("..")[0]),
+                            int(coordrange[2:].split("..")[1]),
+                        ]
+                    )
+                )
+                for coordrange in coordranges.split(",")
+            ],
         )
 
-    def get_points_set(self):
-        result = set()
-        for x in self.xrange:
-            for y in self.yrange:
-                for z in self.zrange:
-                    result.add(Point(x, y, z))
-        return result
+    @cache
+    def in_field(self, p: Point) -> bool:
+        return (
+            self.xrange[0] <= p.x <= self.xrange[1]
+            and self.yrange[0] <= p.y <= self.yrange[1]
+            and self.zrange[0] <= p.z <= self.zrange[1]
+        )
 
 
 core_points: list[Point] = []
@@ -65,9 +66,8 @@ def main() -> None:
     print("Command count:", len(commands))
     for i, command in enumerate(commands):
         print("Processing Command", i)
-        command_points = command.get_points_set()
         for core_point in core_points:
-            if core_point in command_points:
+            if command.in_field(core_point):
                 if command.on:
                     core.add(core_point)
                 else:
